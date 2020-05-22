b0d0f1daead12301d337f864afe6912821e03886
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-05-21 19:56:10 -0700

    removed old or stale code

--- ezgfx-core/src/uniform/mod.rs
+++ ezgfx-core/src/uniform/mod.rs
@@ -1,22 +1,12 @@
 /// implementations of the Uniform trait for common uniform types
 
-mod impl_buffer2;
-mod impl_buffer;
 mod texture;
-mod buffer;
 
 pub use texture::*;
-pub use buffer::*;
 
 /// represents a generic uniform
 pub trait Uniform
 {
-    // /// get the bind group layout of this uniform instance
-    // fn bind_group_layout(&self, stage: wgpu::ShaderStage, ) -> &wgpu::BindGroupLayout;
-
-    // /// get the bind group of this uniform instance
-    // fn bind_group(&self) -> &wgpu::BindGroup;
-
     /// get this uniform's BindingType, which is then used in BindGroupLayoutEntry
     fn ty(&self) -> wgpu::BindingType;
 
